6b34d242868199d0c3df2f4f53598695f6ec522c
André Morsch <andremorsch37@gmail.com>
2023-05-17 15:36:20 -0300

    Create AffiliateData Class
    Cria classe de Afiliados para o banco de dados

---
+++ Backend/BackEnd/Models/AffiliateData.cs
@@ -0,0 +1,12 @@
+﻿namespace BackEnd.Models
+{
+    public class AffiliateData
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Product { get; set; }
+        public decimal Value { get; set; }
+        public string Seller { get; set; }
+    }
+}
