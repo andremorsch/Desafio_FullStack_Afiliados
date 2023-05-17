ba5ee8ae5730e2316fec8be410234cba5cd900e8
André Morsch <andremorsch37@gmail.com>
2023-05-17 00:23:59 -0300

    Create class Transaction
    Classe para criar o objeto da transação individual

---
+++ Backend/BackEnd/Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace BackEnd.Models
+{
+    public class Transaction
+    {
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Product { get; set; }
+        public decimal Value { get; set; }
+        public string Seller { get; set; }
+    }
+}
