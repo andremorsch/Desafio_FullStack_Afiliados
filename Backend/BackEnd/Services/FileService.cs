b1f5d0ce68b5d3dfeb49967c5e3bf672e3e340fa
André Morsch <andremorsch37@gmail.com>
2023-05-17 15:49:21 -0300

    Create ExtractTransactions Method
    Cria método para extrair uma transação de cada vez, criando a lista de transações

--- Backend/BackEnd/Services/FileService.cs
+++ Backend/BackEnd/Services/FileService.cs
@@ -18,5 +18,28 @@ namespace BackEnd.Services
             }
             return null;
         }
+
+        private static List<Transaction> ExtractTransactions(string content)
+        {
+            List<Transaction> transactions = new();
+
+            string[] lines = content.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string line in lines)
+            {
+                Transaction transaction = new();
+                string[] separetaBySpace = line.Split("  ");
+                transaction.Type = separetaBySpace[0].Substring(0, 1);
+                transaction.Date = DateTime.ParseExact(separetaBySpace[0]
+                    .Substring(1, 25), "yyyy-MM-dd'T'HH:mm:ssK", CultureInfo.InvariantCulture);
+                transaction.Product = separetaBySpace[0].Substring(26).Trim();
+                transaction.Value = decimal.Parse(separetaBySpace.Last().Trim().Substring(0, 10)) / 100;
+                transaction.Seller = separetaBySpace.Last().Trim().Substring(10).Trim();
+
+                transactions.Add(transaction);
+            }
+
+            return transactions;
+        }
     }
 }
