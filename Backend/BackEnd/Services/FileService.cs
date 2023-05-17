02fdc39bc61b9bd85208d44626532669ee8d7f29
André Morsch <andremorsch37@gmail.com>
2023-05-17 14:47:52 -0300

    Create method process file
    Cria método para receber e ler o arquivo de texto

---
+++ Backend/BackEnd/Services/FileService.cs
@@ -0,0 +1,22 @@
+﻿using BackEnd.Models;
+using System.Globalization;
+
+namespace BackEnd.Services
+{
+    public class FileService
+    {
+        public static async Task<List<Transaction>?> ProcessFile(TextFile file)
+        {
+            if (file.File != null)
+            {
+                using (var reader = new StreamReader(file.File.OpenReadStream()))
+                {
+                    string content = await reader.ReadToEndAsync();
+                    List<Transaction> transactions = ExtractTransactions(content);
+                    return transactions;
+                }
+            }
+            return null;
+        }
+    }
+}
