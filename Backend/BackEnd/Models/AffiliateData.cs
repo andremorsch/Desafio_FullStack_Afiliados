22ba260e93b7b56496f91bc59c8f16e61369df40
André Morsch <andremorsch37@gmail.com>
2023-05-18 20:28:30 -0300

    update Type
    Atualiza tipo da variável

--- Backend/BackEnd/Models/AffiliateData.cs
+++ Backend/BackEnd/Models/AffiliateData.cs
@@ -3,7 +3,7 @@
     public class AffiliateData
     {
         public int Id { get; set; } = 0;
-        public string Type { get; set; }
+        public int Type { get; set; }
         public DateTime Date { get; set; }
         public string Product { get; set; }
         public decimal Value { get; set; }
