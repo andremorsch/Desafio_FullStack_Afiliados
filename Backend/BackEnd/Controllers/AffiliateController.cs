9f489d14775bd8c5f848c510fe4343be1205ec7f
André Morsch <andremorsch37@gmail.com>
2023-05-18 20:26:28 -0300

    Create AffiliateController.cs
    Cria Controller Post para subir arquivo TXT

---
+++ Backend/BackEnd/Controllers/AffiliateController.cs
@@ -0,0 +1,31 @@
+﻿using BackEnd.Data;
+using BackEnd.Enumerators;
+using BackEnd.Models;
+using BackEnd.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Intrinsics;
+
+namespace BackEnd.Controllers
+{
+    [ApiController]
+    public class AffiliateController : ControllerBase
+    {
+        [HttpPost("v1/processFile/")]
+        public async Task<IActionResult> ProcessFile(
+            [FromForm] TextFile file,
+            [FromServices] AffiliateDataContext context)
+        {
+            if (file?.File != null)
+            {
+                var response = await FileService.ProcessFile(file, context);
+
+                if (response.Success != EnumSuccess.TOTAL_FAILURE)
+                    return Ok();
+
+                return BadRequest("Nenhuma linha foi válida");
+            }
+
+            return BadRequest("Nenhum arquivo enviado");
+        }
+    }
+}
